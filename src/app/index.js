768445315fd9090e6e35e5e1ab3dfbafe2e9cace
Jose Guevara Arce <Jose Luis Guevara@jalasoft.local>
2018-01-27 11:51:46 -0400

    Github structure project

---
+++ src/app/index.js
@@ -0,0 +1,6 @@
+'use strict';
+angular.module('github',
+  ['ngCookies', 'ngSanitize', 'ngResource', 'ngRoute', 'directive'])
+    .config(function ($routeProvider,$httpProvider, $locationProvider) {});
+
+angular.module('directive', []);
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+angular.module('github')
+.controller('MainCtrl', ['$scope', '$route', 'mainService', '$cookies','$window','$location',
+  function ($scope, $route, mainService, $cookies, $window, $location) {
+    //_HERE GOES THE FRONTEND's backend
+  }]);
